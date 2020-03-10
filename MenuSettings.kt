25a6c225778198fb2463d9bf828f03d51917bbc4
DominikSalamon <dominiksalamon1234567890@gmail.com>
2020-03-11 00:37:20 +0100

    Started Map Generetation

---
+++ MenuSettings.kt
@@ -0,0 +1,33 @@
+package com.example.projekt
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_menu_settings.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MenuSettings : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu_settings, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        button_back.setOnClickListener {
+            val fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.setCustomAnimations(R.anim.slide_out_left, R.anim.slide_in_right)
+            fragmentTransaction?.replace(R.id.fragment_container, MenuHome())?.commit()
+        }
+    }
+
+}
